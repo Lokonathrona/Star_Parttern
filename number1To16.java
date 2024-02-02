aa3c7987674c032bbe6521befad9ef88c09c29af
Lokonath Rona <121220332+Lokonathrona@users.noreply.github.com>
2024-02-02 20:57:41 +0530

    Add files via upload

---
+++ number1To16.java
@@ -0,0 +1,16 @@
+package StarParttern;
+
+public class number1To16 {
+	public static void main(String[] args) {
+		int count=1;
+		
+		for(int i=1;i<=4;i++) {
+			for(int j=1;j<=4;j++) {
+				System.out.print(count);
+				count++;
+			}
+			System.out.println();
+		}
+	}
+
+}
